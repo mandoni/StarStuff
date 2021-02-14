b7081db07a27ce40d4009e2e162b56390bc78bce
mandoni <tonytaze096@gmail.com>
2021-02-14 01:33:13 -0600

    NavBar

---
+++ src/Components/Navbar/MenuItem.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title:'Inicio',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Secciones',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Acerca de',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Login',
+        url:'#',
+        cName: 'nav-links-login'
+    },
+    {
+        title:'Crear cuenta',
+        url:'#',
+        cName: 'nav-links-mobile'
+    }
+]
