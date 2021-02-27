f3fc1c07813965e8f648fa3d8cf1a9d4608a77c9
mandoni <tonytaze096@gmail.com>
2021-02-26 19:30:57 -0600

    CRUD - funcional desde Postman

--- api/controllers/postDocumentController.js
+++ api/controllers/postDocumentController.js
@@ -1,12 +1,59 @@
 const express = require('express')
 var router = express.Router()
 
-var { PostDocument } = require('../models/postDocument')
+var { postDocument } = require('../models/postDocument')
 
-//CREAR 
-router.get('/', (req, res) => {
-    PostDocument.find((err, docs) => {
+
+router.get('/', (req, res) => { 
+    postDocument.find((err, docs) => {
         if (!err) res.send(docs)
-        else console.log('Error while retrieving all records : ' + JSON.stringify(err, undefined, 2))
+        else console.log('Error al leer los registros: ' + JSON.stringify(err, undefined, 2))
     })
 })
+
+router.post('/', (req, res) => {
+    var newRecord = new postDocument({
+        title: req.body.title,
+        autor: req.body.autor,
+        fecha: req.body.fecha,
+        encabezado : req.body.encabezado,
+        seccion : req.body.seccion,
+        documento : req.body.documento,
+        urlImg : req.body.urlImg,
+        fuente : req.body.fuente,
+        urlFuente : req.body.urlFuente
+    })
+
+    newRecord.save((err, docs) => {
+        if (!err) res.send(docs)
+        else console.log('Error al crear registro: ' + JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.put('/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('Registro no encontrado : ' + req.params.id)
+
+    var updatedRecord = {
+        title: req.body.title,
+        message: req.body.message
+    }
+
+    postDocument.findByIdAndUpdate(req.params.id, { $set: updatedRecord },{new:true}, (err, docs) => {
+        if (!err) res.send(docs)
+        else console.log('Error al actualizar el registro: ' + JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID de registro no encontrado: ' + req.params.id)
+
+        postDocument.findByIdAndRemove(req.params.id, (err, docs) => {
+        if (!err) res.send(docs)
+        else console.log('Error al eliminer el registro: ' + JSON.stringify(err, undefined, 2))
+    })
+})
+
+
+module.exports = router
--- api/models/postDocument.js
+++ api/models/postDocument.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose')
 var postDocument = mongoose.model('postDocument',
 {
     title : {type:String},
+    autor : {type:String},
+    fecha : {type:String},
     encabezado : {type:String},
     seccion : {type:String},
     documento : {type:String},
     urlImg : {type:String},
-    urlFuente : {type:String},
+    fuente : {type:String},
+    urlFuente : {type:String}
 }/*,'postDocument'*/)
 
 module.exports = { postDocument }
