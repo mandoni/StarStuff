81d3600a50a2bb33d29de3d045eec7b164e1a07a
mandoni <tonytaze096@gmail.com>
2021-04-08 18:37:42 -0600

    Login bloqueado si ya esta logeado

--- src/Components/Login/Signup.jsx
+++ src/Components/Login/Signup.jsx
@@ -1,12 +1,10 @@
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import React, { useState } from "react";
-import { AirlineSeatIndividualSuiteRounded, Mail } from '@material-ui/icons'
 import loginImg from '../../Media/login-logo.svg'
 import authImg from '../../Media/register-logo.svg'
-import classnames from "classnames";
 import Input from "./Input"
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { GoogleLogin } from 'react-google-login'
 import Ico from "./icon"
 import { signin, signup } from "../../Actions/auth";
@@ -24,6 +22,7 @@ const initialState = {
 
 export const Signup = () => {
 
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
     const [form, setForm] = useState(initialState)
@@ -74,57 +73,128 @@ export const Signup = () => {
 
     return (
         <div>
-            <Container component="main" maxWidth="xs">
-                <Paper className={classes.paper} elevation={3}>
-                    <img src={isSignup ? loginImg : authImg} className="dim-img" alt="Imagen de Login" />
-                    <Typography variant="h5">{isSignup ? 'Crear cuenta' : 'Iniciar sesión'}</Typography>
-                    <form onSubmit={handleSubmmi} className={classes.form}>
-                        <Grid container spacing={2}>
-                            {
-                                isSignup && (
-                                    <>
-                                        <Input name="firstName" label="Nombre" handleChange={handleChange} autoFocus half />
-                                        <Input name="lastName" label="Apellido" handleChange={handleChange} half />
-                                    </>
-                                )
-                            }
-                            <Input name="correo" label="Correo electrónico" handleChange={handleChange} type="email" />
-                            <Input name="password" label="Contraseña" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-                            {
-                                isSignup && <Input name="confirmPassword" label="Confirmar " handleChange={handleChange} type="password" />
-                            }
+            {(user && user.result) ?
+                (
+                    <div className="front-sin-cuenta">
+                        <Grid className="crear-cuenta">
+                            <Typography
+                                variant="h4"
+                                align="center"
+                            >
+                                YA ESTÁS LOGEADO
+                        </Typography>
+                            <Link to="/perfil">
+                                <button className="btn">Ir a perfil</button>
+                            </Link>
                         </Grid>
-                        <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-                            {isSignup ? "Crear" : "Entrar"}
-                        </Button>
-                        <GoogleLogin
-                            clientId="447319009478-a0bvu7re26u7c9bjgl3h7ufhg3ks9sod.apps.googleusercontent.com"
-                            render={(renderProps) => (
-                                <Button
-                                    className={classes.googleButton}
-                                    color="secondary"
-                                    fullWidth
-                                    onClick={renderProps.onClick}
-                                    disabled={renderProps.disabled}
-                                    startIcon={<Ico />}
-                                    variant="contained"> Google
+                        <div className="footer-container">
+                            <section class='social-media'>
+                                <div class='social-media-wrap'>
+                                    <div class='footer-logo'>
+                                        <a href="#inicio" className='social-logo'>
+                                            StarStuff
+                                        <i class='fas fa-globe-americas' />
+                                        </a>
+                                    </div>
+                                    <small class='website-rights'>NorYu © 2021</small>
+                                    <div class='social-icons'>
+                                        <Link
+                                            class='social-icon-link facebook'
+                                            to='/'
+                                            target='_blank'
+                                            aria-label='Facebook'
+                                        >
+                                            <i class='fab fa-facebook-f' />
+                                        </Link>
+                                        <Link
+                                            class='social-icon-link instagram'
+                                            to='/'
+                                            target='_blank'
+                                            aria-label='Instagram'
+                                        >
+                                            <i class='fab fa-instagram' />
+                                        </Link>
+                                        <Link
+                                            class='social-icon-link youtube'
+                                            to='/'
+                                            target='_blank'
+                                            aria-label='Youtube'
+                                        >
+                                            <i class='fab fa-youtube' />
+                                        </Link>
+                                        <Link
+                                            class='social-icon-link twitter'
+                                            to='/'
+                                            target='_blank'
+                                            aria-label='Twitter'
+                                        >
+                                            <i class='fab fa-twitter' />
+                                        </Link>
+                                        <Link
+                                            class='social-icon-link twitter'
+                                            to='/'
+                                            target='_blank'
+                                            aria-label='LinkedIn'
+                                        >
+                                            <i class='fab fa-linkedin' />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                ) : (
+                    <Container component="main" maxWidth="xs">
+                        <Paper className={classes.paper} elevation={3}>
+                            <img src={isSignup ? loginImg : authImg} className="dim-img" alt="Imagen de Login" />
+                            <Typography variant="h5">{isSignup ? 'Crear cuenta' : 'Iniciar sesión'}</Typography>
+                            <form onSubmit={handleSubmmi} className={classes.form}>
+                                <Grid container spacing={2}>
+                                    {
+                                        isSignup && (
+                                            <>
+                                                <Input name="firstName" label="Nombre" handleChange={handleChange} autoFocus half />
+                                                <Input name="lastName" label="Apellido" handleChange={handleChange} half />
+                                            </>
+                                        )
+                                    }
+                                    <Input name="correo" label="Correo electrónico" handleChange={handleChange} type="email" />
+                                    <Input name="password" label="Contraseña" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                                    {
+                                        isSignup && <Input name="confirmPassword" label="Confirmar " handleChange={handleChange} type="password" />
+                                    }
+                                </Grid>
+                                <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                                    {isSignup ? "Crear" : "Entrar"}
                                 </Button>
-                            )}
-                            onSuccess={googleSuccess}
-                            onFailure={googleError}
-                            cookiePolicy="single_host_origin"
-                        />
-                        <Grid container justify="flex-end">
-                            <Grid item>
-                                <Button onClick={switchMode}>
-                                    {isSignup ? "¿Ya tienes una cuenta?" : "¿No tienes una cuenta? Registrate"}
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </Paper>
-            </Container>
-
+                                <GoogleLogin
+                                    clientId="447319009478-a0bvu7re26u7c9bjgl3h7ufhg3ks9sod.apps.googleusercontent.com"
+                                    render={(renderProps) => (
+                                        <Button
+                                            className={classes.googleButton}
+                                            color="secondary"
+                                            fullWidth
+                                            onClick={renderProps.onClick}
+                                            disabled={renderProps.disabled}
+                                            startIcon={<Ico />}
+                                            variant="contained"> Google
+                                        </Button>
+                                    )}
+                                    onSuccess={googleSuccess}
+                                    onFailure={googleError}
+                                    cookiePolicy="single_host_origin"
+                                />
+                                <Grid container justify="flex-end">
+                                    <Grid item>
+                                        <Button onClick={switchMode}>
+                                            {isSignup ? "¿Ya tienes una cuenta?" : "¿No tienes una cuenta? Registrate"}
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </Paper>
+                    </Container>
+                )}
         </div>
     )
 }
